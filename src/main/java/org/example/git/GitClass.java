9c67adbea4672401cab87beef63d0c9d37e16cd7
pk <125@qq.com>
2022-06-21 19:03:38 +0800

    hot-fix commit1

--- src/main/java/org/example/git/GitClass.java
+++ src/main/java/org/example/git/GitClass.java
@@ -4,5 +4,6 @@ public class GitClass {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("he");
+        System.out.println("hello git3");
     }
 }
